8aefd6bed111ee35ada2ef9b9dd9d19bdaffa324
Jung Geon Choi <jung.choi@pointclickcare.com>
2022-09-27 14:21:04 -0400

    asdf

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,2 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
-
-void main() {
-  runApp(const IAMLoginPOC());
-}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:mobile_iam_poc/views/login_view.dart';
 import 'package:mobile_iam_poc/views/pw_resident_list.dart';
 
+void main() {
+  runApp(const IAMLoginPOC());
+}
+
 class IAMLoginPOC extends StatelessWidget {
   const IAMLoginPOC({Key? key}) : super(key: key);
 
